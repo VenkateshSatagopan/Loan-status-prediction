d443df891c17d1145879e264417f92376ee4a245
Venkatesh Satagopan <venkateshsatagopan@gmail.com>
2020-12-30 16:50:49 +0100

    Modified app file to handle single and batch file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,jsonify
+from flask import Flask, render_template,request,jsonify,redirect
 import pandas as pd
 import pickle
 import numpy as np
@@ -7,22 +7,64 @@ import pandas as pd
 import joblib
 import catboost
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier,GradientBoostingClassifier,ExtraTreesClassifier
+from werkzeug.utils import secure_filename
 import xgboost
-
+import os
+import time
 
 model=joblib.load('best-model-loan-prediction.pkl')
 
 app=Flask(__name__)
 
-
 @app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        try: 
+            if request.form['Single Data Prediction']=='single data':
+                return redirect('single_data_prediction')
+        except:
+            pass
+        try: 
+            if request.form['Batch Data Prediction']=='batch data':
+                return redirect('batch_data_prediction')
+        except:
+            pass
+
+    return render_template('home.html')
+
+@app.route('/batch_data_prediction',methods=['GET','POST'])
+def bacth_prediction():
+    if request.method =='POST':
+        d=request.form.to_dict()
+        
+        csv_file=list(d.values())[0]
+        print(csv_file)
+        try:
+            if csv_file:
+              df=pd.read_csv(csv_file)
+              
+              df['prediction']=model.predict(df.drop(['Loan_ID'],axis=1))
+              print(df.head())
+              basepath = os.path.dirname(__file__)
+              print(basepath)
+              timestr = time.strftime("%Y%m%d-%H%M%S")
+              final_csv_file='prediction_'+str(timestr)+'.csv'
+              print(final_csv_file)
+              file_path = os.path.join(basepath, 'static',  final_csv_file)
+              print(file_path)
+              df.to_csv(file_path,index=False)
+              return render_template("index_1.html",prediction='The model prediction is done and the predicted results is saved in the path {} under the name {}'.format( os.path.join(basepath, 'static'),secure_filename(final_csv_file)))
+        except Exception as e:
+            return render_template("index_1.html",prediction='Please enter the csv file correctly')
+
+    return render_template("index_1.html",prediction='No data has been provided')        
+
+@app.route('/single_data_prediction',methods=['GET','POST'])
 def get_details():
     d = None
     if request.method=='POST':
         d = request.form.to_dict()
         df = pd.DataFrame([d.values()], columns=d.keys())
-        #df['Unnamed: 0']=-1
-        #print(df.info())
         df['Gender']=df['Gender'].astype('str')
         df['Married']=df['Married'].astype('str')
         df['Dependents']=df['Dependents'].astype('str')
